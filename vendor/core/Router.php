decec8b9033dfbcf38e401007fb6e37e2be856b4
nimbler <yazovcev@gmail.com>
2017-04-15 17:10:48 +0300

    Создание базовых переменных и функций класса Router

--- vendor/core/Router.php
+++ vendor/core/Router.php
@@ -6,4 +6,21 @@
 class Router
 {
 
+  protected static $routes = [];  // массив таблицы маршрутов
+  protected static $route = [];   // текущий маршрут
+
+  public static function add($regexp, $route = [])
+  {
+    self::$routes[$regexp] = $route;
+  }
+
+  public static function getRoutes()
+  {
+    return self::$routes;
+  }
+
+  public static function getRoute()
+  {
+    return self::$route;
+  }
 }
