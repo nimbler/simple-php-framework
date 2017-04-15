563c78a82364d617d1b1f1dcd32a6ed8d9051f70
nimbler <yazovcev@gmail.com>
2017-04-15 23:17:14 +0300

    Помещаем содержимое вида в буфер
    Signed-off-by: nimbler <yazovcev@gmail.com>

--- vendor/core/base/View.php
+++ vendor/core/base/View.php
@@ -36,10 +36,19 @@ class View
   public function render()
   {
     $file_view = APP . "/views/{$this->route['controller']}/{$this->view}.php";
+    ob_start();
     if (is_file($file_view)) {
       require $file_view;
     } else {
       echo "<p>Не найден вид <b>{$file_view}</b></p>";
+    }
+    $content = ob_get_clean();
+
+    $file_layout = APP . "/views/layouts/{$this->layout}.php";
+    if (is_file($file_layout)) {
+      require $file_layout;
+    } else {
+      echo "<p>Не найден шаблон <b>$file_layout</b></p>";
     }
   }
 }
